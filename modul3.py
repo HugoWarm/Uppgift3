15379ed54787879c8d9e2f2c4f7c308d8c6fc009
HugoWarm <hugowarmland36@gmail.com>
2026-09-04 13:31:12 +0200

    nästa

--- modul3.py
+++ modul3.py
@@ -1,5 +1,15 @@
-height= float(input("hur lång är du i meter?"))
-if height < 1.40:
-    print("Du kan inte åka på gröna lund")
-else:
-    print("Du kan åka på gröna lund")
+# height= float(input("Hur lång är du i meter?"))
+# if height < 1.40 or height > 1.95:
+#     print("Du kan inte åka på gröna lund")
+# else:
+#     print("Du kan åka på gröna lund")
+
+try:
+    förnamn= input("Vad är ditt förnamn?")
+    efternamn= input("Vad är ditt efternamn?")
+   
+    print("Du heter "+ förnamn +" "+ efternamn + " och du är ")#+ int(age) + " år gammal")
+except:
+    print("Fel format, skriv bara bokstäver på den första och andra och nummer på den tredje")
+
+     #age= input("Hur gammal är du?")

7fafb3b0dbef75040f7a76a4ded16d596bab7c2f
HugoWarm <hugowarmland36@gmail.com>
2026-09-10 11:16:23 +0200

    Fortsatte

--- modul3.py
+++ modul3.py
@@ -1,15 +1,36 @@
+import random
+
 # height= float(input("Hur lång är du i meter?"))
 # if height < 1.40 or height > 1.95:
 #     print("Du kan inte åka på gröna lund")
 # else:
 #     print("Du kan åka på gröna lund")
 
-try:
-    förnamn= input("Vad är ditt förnamn?")
-    efternamn= input("Vad är ditt efternamn?")
-   
-    print("Du heter "+ förnamn +" "+ efternamn + " och du är ")#+ int(age) + " år gammal")
-except:
-    print("Fel format, skriv bara bokstäver på den första och andra och nummer på den tredje")
+# förnamn= input("Vad är ditt förnamn?")
+# efternamn= input("Vad är ditt efternamn?")
 
-     #age= input("Hur gammal är du?")
+# try:
+#     age= int(input("Hur gammal är du?"))
+   
+#     print("Du heter "+ förnamn +" "+ efternamn + " och du är ")
+#     print("Du är", age, "år gammal")
+# except:
+#     print("Fel format, skriv bara nummer")
+
+
+# print("Skriv in din vikt i kg och längd i meter så kan vi räkna ut ditt bmi.")
+# try:
+#     längd= float(input("Din längd i meter"))
+#     vikt= int(input("Din vikt i kg"))
+#     print("Din bmi är",vikt/längd**2)
+# except:
+#     print("Skriv längd i meter och vikt i kg.")
+
+
+# try:
+#     radius = float(input("Skriv radien på din cirkel i cm."))
+#     print("Din cirkel har arean",3.14*radius**2,"cm^2")
+# except:
+#     print("Skiv inte bokstäver.")
+
+
